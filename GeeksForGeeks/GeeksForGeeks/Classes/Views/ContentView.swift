33b9d444b3aec517848f5a58664246fb016c442c
simranrout <simranrout17@gmail.com>
2022-05-26 12:53:00 +0530

    Completed Project Set Up and folder structure

---
+++ GeeksForGeeks/GeeksForGeeks/Classes/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeeksForGeeks
+//
+//  Created by Simran Rout on 26/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack() {
+            HeaderView()
+            Spacer()
+            }
+            Spacer()
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeeksForGeeks/GeeksForGeeks/Classes/Views/GeeksForGeeksApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeeksForGeeksApp.swift
+//  GeeksForGeeks
+//
+//  Created by Simran Rout on 26/05/22.
+//
+
+import SwiftUI
+
+@main
+struct GeeksForGeeksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
